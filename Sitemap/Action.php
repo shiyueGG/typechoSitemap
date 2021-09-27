8d9fdfc1727c5ba0dfda9c07f7d62c3f6c97162b
shiyue <58288932+shiyueGG@users.noreply.github.com>
2021-09-27 11:12:21 +0800

    1.0.1
    支持开启分级显示和不开启分级，
    分级是指sitemap分多个地址显示，适合数据量大的站点

---
+++ Sitemap/Action.php
@@ -0,0 +1,343 @@
+<?php
+
+/**
+ * Sitemap Plugin
+ *
+ * @copyright  Copyright (c) 2021 十月 (https://Oct.cn)
+ * 
+ */
+class Sitemap_Action extends Typecho_Widget implements Widget_Interface_Do
+{
+	public function __construct($request, $response, $params = NULL)
+	{
+		parent::__construct($request, $response, $params);
+		$this->db = Typecho_Db::get();
+		$this->siteUrl = Typecho_Widget::widget('Widget_Options')->siteUrl;
+		$this->Sitemap = Typecho_Widget::widget('Widget_Options')->Plugin('Sitemap');
+		$siteStatus = $this->Sitemap->siteStatus;
+		$this->sitePageSize = $this->Sitemap->sitePageSize;
+		$this->pageSize = Typecho_Widget::widget('Widget_Options')->pageSize;
+		$this->ymd = date('Y-m-d', time());
+		if ($siteStatus == 0) {
+			// 关闭了sitemap
+			$this->checkData();
+		}
+	}
+
+	/**
+	 * sitemap
+	 * 
+	 */
+	public function siteMap()
+	{
+		// 不分级
+		if ($this->Sitemap->levelSite == 1) {
+			$this->levelSiteList();
+		} else {
+			$this->siteListXml();
+		}
+	}
+
+
+	/**
+	 * 不分级
+	 * 
+	 */
+	public function levelSiteList()
+	{
+		$xmlhtml = "<url><loc> " . $this->siteUrl . "</loc><lastmod> " . $this->ymd . " </lastmod><changefreq> daily </changefreq><priority> 1.0 </priority></url>";
+		// 分类
+		$obj = $this->widget('Widget_Metas_Category_List');
+		if ($obj->have()) {
+			while ($obj->next()) {
+				$xmlhtml .= "<url><loc> " . $obj->permalink . "</loc><lastmod> " . $this->ymd . " </lastmod><changefreq> daily </changefreq><priority> " . $this->Sitemap->CatePagePriority . " </priority></url>";
+			}
+		}
+		// 单页
+		$obj = $this->widget('Widget_Contents_Page_List');
+		if ($obj->have()) {
+			while ($obj->next()) {
+				$xmlhtml .= "<url><loc> " . $obj->permalink . "</loc><lastmod> " . $this->ymd . " </lastmod><changefreq> daily </changefreq><priority> " . $this->Sitemap->pagesPriority . " </priority></url>";
+			}
+		}
+		// 标签
+		$xmlhtml .= $this->retTag(true);
+		// 文章
+		$xmlhtml .= $this->retPost(true);
+		// 分页
+		$xmlhtml .= $this->retHomePage(true);
+		$this->showXml($xmlhtml);
+	}
+
+	/**
+	 * 分级
+	 * 
+	 */
+	public function siteListXml()
+	{
+		$allSrat = Typecho_Widget::widget('Widget_Stat');
+		// 文章总数
+		$postCount = $allSrat->publishedPostsNum;
+		// 标签总数
+		$tagsCount =  $this->db->fetchObject($this->db->select(array('COUNT(mid)' => 'num'))
+			->from('table.metas')
+			->where('table.metas.type = ?', 'tag'))->num;
+		$tagsCount = intval($tagsCount);
+
+		// 分布
+		$xmlhtml = "<url><loc> " . $this->siteUrl .  "</loc><lastmod> " . $this->ymd . " </lastmod><changefreq> daily </changefreq><priority> 1.0 </priority></url>";
+
+		// 文章分页
+		$postSize = ceil($postCount / $this->sitePageSize);
+		for ($i = 0; $i < $postSize; $i++) {
+			$xmlhtml .= "<url><loc> " . $this->siteUrl .  "sitemap/sitemap_post_" . ($i + 1) . ".xml</loc><lastmod> " . $this->ymd . " </lastmod><changefreq> daily </changefreq><priority> 1.0 </priority></url>";
+		}
+
+		// 标签分页
+		$tagsSize = ceil($tagsCount / $this->sitePageSize);
+		for ($i = 0; $i < $tagsSize; $i++) {
+			$xmlhtml .= "<url><loc> " . $this->siteUrl .  "sitemap/sitemap_tag_" . ($i + 1) . ".xml</loc><lastmod> " . $this->ymd . " </lastmod><changefreq> daily </changefreq><priority> 1.0 </priority></url>";
+		}
+
+		// 独立页面
+		$xmlhtml .= "<url><loc> " . $this->siteUrl .  "sitemap/sitemap_pages.xml</loc><lastmod> " . $this->ymd . " </lastmod><changefreq> daily </changefreq><priority> 1.0 </priority></url>";
+
+		// 首页翻页分页
+		$pageSize = ceil(($postCount / $this->pageSize) / $this->sitePageSize);
+		for ($i = 0; $i < $pageSize; $i++) {
+			$xmlhtml .= "<url><loc> " . $this->siteUrl .  "sitemap/sitemap_homepage_" . ($i + 1) . ".xml</loc><lastmod> " . $this->ymd . " </lastmod><changefreq> daily </changefreq><priority> 1.0 </priority></url>";
+		}
+
+		// 分类总数
+		$cateList = $this->db->fetchAll($this->db->select('mid')->from('table.metas')
+			->where('table.metas.type = ?', 'category')
+			->order('table.metas.mid', Typecho_Db::SORT_DESC));
+		for ($i = 0; $i < count($cateList); $i++) {
+			// 分类分页
+			$xmlhtml .= "<url><loc> " . $this->siteUrl .  "sitemap/sitemap_catepage_" . $cateList[$i]['mid'] . ".xml</loc><lastmod> " . $this->ymd . " </lastmod><changefreq> daily </changefreq><priority> 1.0 </priority></url>";
+		}
+
+		$this->showXml($xmlhtml);
+		$this->xmlJson([
+			'标签总数' => $tagsCount,
+			'标签页数' => $tagsSize,
+			'文章总数' => $postCount,
+			'文章页数' => $postSize,
+			'首页翻页页数' => $pageSize,
+			'分页数' => $this->sitePageSize,
+			'分类总数' => $cateList,
+		]);
+	}
+
+
+	/**
+	 * 定向xml
+	 * 
+	 */
+	public function siteList()
+	{
+		$key = $this->request->key;
+		if ($key == 'post') {
+			$this->retPost();
+		} else if ($key == 'tag') {
+			$this->retTag();
+		} else if ($key == 'cate') {
+			$this->retCate();
+		} else if ($key == 'homepage') {
+			$this->retHomePage();
+		} else if ($key == 'catepage') {
+			$this->retCatePage();
+		} else if ($key == 'pages') {
+			$this->retPages();
+		} else {
+			$this->checkData();
+		}
+	}
+
+	/**
+	 * 单页
+	 * 
+	 */
+	public function retPages()
+	{
+		$xmlhtml = '';
+		$pages = $this->widget('Widget_Contents_Page_List');
+		$data = [];
+		if ($pages->have()) {
+			while ($pages->next()) {
+				$data[] = $pages->permalink;
+			}
+		}
+		// 效验
+		$this->checkData($data);
+		$priority = $this->Sitemap->pagesPriority;
+		for ($i = 0; $i < count($data); $i++) {
+			$xmlhtml .= "<url><loc> " . $data[$i] . "</loc><lastmod> " . $this->ymd . " </lastmod><changefreq> daily </changefreq><priority> " . $priority . " </priority></url>";
+		}
+		$this->showXml($xmlhtml);
+	}
+
+	/**
+	 * 指定分类下的页数
+	 * 
+	 */
+	public function retCatePage()
+	{
+		$page = $this->request->page;
+		$xmlhtml = '';
+		$count =  $this->db->fetchObject($this->db->select(array('COUNT(mid)' => 'num'))
+			->from('table.relationships')
+			->where('table.relationships.mid = ?', $page))->num;
+		$count = intval($count);
+		// 效验
+		$this->checkData($count);
+		$data = [];
+		$obj = $this->widget('Widget_Metas_Category_List');
+		if ($obj->have()) {
+			while ($obj->next()) {
+				if ($obj->mid == $page) {
+					$data = [
+						'mid' => $obj->mid,
+						'permalink' => $obj->permalink,
+						'slug' => $obj->slug,
+					];
+				}
+			}
+		}
+
+		$xmlhtml = "<url><loc> " . $data['permalink'] . "</loc><lastmod> " . $this->ymd . " </lastmod><changefreq> daily </changefreq><priority> " . $this->Sitemap->catePriority . " </priority></url>";
+
+		// 分页
+		$count = ceil($count / $this->pageSize);
+		$priority = $this->Sitemap->CatePagePriority;
+		for ($i = 1; $i < $count; $i++) {
+			$xmlhtml .= "<url><loc> " . $data['permalink'] . "/" . $i . "/</loc><lastmod> " . $this->ymd . " </lastmod><changefreq> daily </changefreq><priority> " . $priority . " </priority></url>";
+		}
+		$this->showXml($xmlhtml);
+	}
+
+	/**
+	 * 首页分页
+	 * 
+	 */
+	public function retHomePage($ret = null)
+	{
+		$postCount = Typecho_Widget::widget('Widget_Stat')->publishedPostsNum;
+		$limit = $this->pageSize;
+		$pages = ceil($postCount / $limit);
+		$xmlhtml = '';
+		$priority = $this->Sitemap->HomePagePriority;
+		for ($i = 1; $i < $pages; $i++) {
+			$xmlhtml .= "<url><loc> " . $this->siteUrl . "page/" . $i . "/</loc><lastmod> " . $this->ymd . " </lastmod><changefreq> daily </changefreq><priority> " . $priority . " </priority></url>";
+		}
+
+		if ($ret) {
+			return $xmlhtml;
+		}
+		$this->showXml($xmlhtml);
+	}
+
+	/**
+	 * tag
+	 * 
+	 */
+	public function retTag($ret = null)
+	{
+		$page = $this->request->page;
+		$xmlhtml = '';
+		$limit = $this->sitePageSize;
+		if ($ret) {
+			$page = 0;
+			$limit = 10000;
+		}
+		$tags = $this->db->fetchAll($this->db->select('slug,mid')->from('table.metas')
+			->where('table.metas.type = ?', 'tag')
+			->page($page, $limit)
+			->order('table.metas.mid', Typecho_Db::SORT_ASC));
+		// 效验
+		if (!$ret) {
+			$this->checkData($tags);
+		}
+		$priority = $this->Sitemap->tagPriority;
+		for ($i = 1; $i < count($tags); $i++) {
+			$xmlhtml .= "<url><loc> " . $this->siteUrl . "tag/" . urlencode($tags[$i]['slug']) . "</loc><lastmod> " . $this->ymd . " </lastmod><changefreq> daily </changefreq><priority> " . $priority . " </priority></url>";
+		}
+
+		if ($ret) {
+			return $xmlhtml;
+		}
+		$this->showXml($xmlhtml);
+	}
+
+	/**
+	 * 文章
+	 * 
+	 */
+	public function retPost($ret = null)
+	{
+		$page = $this->request->page;
+		$xmlhtml = '';
+		$limit =  $this->sitePageSize;
+		if ($ret) {
+			$page = 0;
+			$limit = 10000;
+		}
+		$content = $this->db->fetchAll($this->db->select('cid,slug,created')->from('table.contents')
+			->where('table.contents.status = ?', 'publish')
+			->where('table.contents.type = ?', 'post')
+			->page($page, $limit)
+			->order('table.contents.cid', Typecho_Db::SORT_ASC));
+		// 效验
+		if (!$ret) {
+			$this->checkData($content);
+		}
+		$priority = $this->Sitemap->postPriority;
+		$options = Typecho_Widget::widget('Widget_Options');
+		foreach ($content as $v) {
+			$v['slug'] = urlencode($v['slug']);
+			$v['date'] = new Typecho_Date($v['created']);
+			$v['year'] = $v['date']->year;
+			$v['month'] = $v['date']->month;
+			$v['day'] = $v['date']->day;
+			$type = 'post';
+			$routeExists = (NULL != Typecho_Router::get($type));
+			$v['pathinfo'] = $routeExists ? Typecho_Router::url($type, $v) : '#';
+			$v['permalink'] = Typecho_Common::url($v['pathinfo'], $options->index);
+			$xmlhtml .= "<url><loc> " . $v['permalink'] . "</loc><lastmod> " . date('Y-m-d H:i:s', $v['created']) . " </lastmod><changefreq> daily </changefreq><priority> " . $priority . " </priority></url>";
+		}
+		if ($ret) {
+			return $xmlhtml;
+		}
+		$this->showXml($xmlhtml);
+	}
+
+	// 入口
+	public function action()
+	{
+		$this->widget('Widget_User')->pass('administrator');
+	}
+
+
+	public function showXml($xmlhtml)
+	{
+		header("Content-type: text/xml");
+		echo '<?xml version="1.0" encoding="utf-8"?><urlset>';
+		echo $xmlhtml;
+		echo '</urlset>';
+		exit();
+	}
+
+	public function xmlJson($data)
+	{
+		header('Content-Type:application/json; charset=utf-8');
+		exit(json_encode($data, JSON_UNESCAPED_UNICODE));
+	}
+
+	public function checkData($data = null)
+	{
+		if (!$data) {
+			throw new Typecho_Widget_Exception(_t('页面不存在'), 404);
+		}
+		return true;
+	}
+}
---
+++ Sitemap/Plugin.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * 全站Sitemap.xml 高度自定义设置 <br>
+ * 请手动在robots.txt添加<a target="_blank" href="/sitemap.xml">sitemap.xml</a>指引 <br>
+ * 设置教程请访问：<a style="font-weight:bold;" href="https://Oct.cn/view/66">教程地址>></a>
+ *
+ * @package Sitemap
+ * @author 十月 Oct.cn
+ * @version 1.0.1
+ * @link https://Oct.cn/view/66
+ */
+class Sitemap_Plugin implements Typecho_Plugin_Interface
+{
+	/**
+	 * 激活插件方法,如果激活失败,直接抛出异常
+	 *
+	 * @access public
+	 * @return String
+	 * @throws Typecho_Plugin_Exception
+	 */
+	public static function activate()
+	{
+		$db = Typecho_Db::get();
+		Helper::addRoute('sitemap', '/sitemap.xml', 'Sitemap_Action', 'siteMap');
+		Helper::addRoute('sitemap/sitemap_[key]', '/sitemap/sitemap_[key].xml', 'Sitemap_Action', 'siteList');
+		Helper::addRoute('sitemap/sitemap_[key]_[page]', '/sitemap/sitemap_[key]_[page].xml', 'Sitemap_Action', 'siteList');
+		return ('开启成功, 插件已经成功激活!');
+	}
+	/**
+	 * 禁用插件方法,如果禁用失败,直接抛出异常
+	 *
+	 * @static
+	 * @access public
+	 * @return String
+	 * @throws Typecho_Plugin_Exception
+	 */
+	public static function deactivate()
+	{
+		Helper::removeRoute('sitemap');
+		Helper::removeAction('sitemap/sitemap_[key]');
+		Helper::removeAction('sitemap/sitemap_[key]_[page]');
+		return ('插件已禁用');
+	}
+	/**
+	 * 获取插件配置面板
+	 *
+	 * @access public
+	 * @param Typecho_Widget_Helper_Form $form 配置面板
+	 * @return void
+	 */
+	public static function config(Typecho_Widget_Helper_Form $form)
+	{
+		$siteStatus =  new Typecho_Widget_Helper_Form_Element_Radio('siteStatus', array('1' => _t('开启'), '0' => _t('关闭')), '1', _t('开启sitemap.xml'), _t('开启后对收录更友好'));
+        $form->addInput($siteStatus);
+        $levelSite =  new Typecho_Widget_Helper_Form_Element_Radio('levelSite', array('1' => _t('不开启分级'), '0' => _t('开启分级')), '1', _t('是否分多个xml文件'), _t('百度不建议分级，但分级也可以收录。若数据量很大，打开缓慢时建议开启'));
+		$form->addInput($levelSite);
+		$sitePageSize =  new Typecho_Widget_Helper_Form_Element_Radio('sitePageSize', array('200' => _t('200'), '500' => _t('500'), '1000' => _t('1000')), '200', _t('每页最多可显示'), _t('仅在开启分级下生效，建议200条,超出自动分页'));
+		$form->addInput($sitePageSize);
+		$catePriority =  new Typecho_Widget_Helper_Form_Element_Radio('catePriority', array('1' => _t('1'), '0.9' => _t('0.9'), '0.8' => _t('0.8'), '0.7' => _t('0.7'), '0.6' => _t('0.6'), '0.5' => _t('0.5')), '0.9', _t('分类页优先级'));
+		$form->addInput($catePriority);
+		$tagPriority =  new Typecho_Widget_Helper_Form_Element_Radio('tagPriority', array('1' => _t('1'), '0.9' => _t('0.9'), '0.8' => _t('0.8'), '0.7' => _t('0.7'), '0.6' => _t('0.6'), '0.5' => _t('0.5')), '0.8', _t('标签页优先级'));
+		$form->addInput($tagPriority);
+		$postPriority =  new Typecho_Widget_Helper_Form_Element_Radio('postPriority', array('1' => _t('1'), '0.9' => _t('0.9'), '0.8' => _t('0.8'), '0.7' => _t('0.7'), '0.6' => _t('0.6'), '0.5' => _t('0.5')), '0.9', _t('文章页优先级'));
+		$form->addInput($postPriority);
+		$HomePagePriority =  new Typecho_Widget_Helper_Form_Element_Radio('HomePagePriority', array('1' => _t('1'), '0.9' => _t('0.9'), '0.8' => _t('0.8'), '0.7' => _t('0.7'), '0.6' => _t('0.6'), '0.5' => _t('0.5')), '0.8', _t('首页分页优先级'));
+		$form->addInput($HomePagePriority);
+		$CatePagePriority =  new Typecho_Widget_Helper_Form_Element_Radio('CatePagePriority', array('1' => _t('1'), '0.9' => _t('0.9'), '0.8' => _t('0.8'), '0.7' => _t('0.7'), '0.6' => _t('0.6'), '0.5' => _t('0.5')), '0.7', _t('分类分页优先级'));
+		$form->addInput($CatePagePriority);
+		$pagesPriority =  new Typecho_Widget_Helper_Form_Element_Radio('pagesPriority', array('1' => _t('1'), '0.9' => _t('0.9'), '0.8' => _t('0.8'), '0.7' => _t('0.7'), '0.6' => _t('0.6'), '0.5' => _t('0.5')), '0.8', _t('独立页面优先级'));
+		$form->addInput($pagesPriority);
+	}
+	/**
+	 * 个人用户的配置面板
+	 *
+	 * @access public
+	 * @param Typecho_Widget_Helper_Form $form
+	 * @return void
+	 */
+	public static function personalConfig(Typecho_Widget_Helper_Form $form)
+	{
+	}
+}
