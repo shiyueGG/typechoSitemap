9edd994b2419691b0d93cb8426335a51a56f3ec6
shiyue <58288932+shiyueGG@users.noreply.github.com>
2021-09-28 16:00:36 +0800

    1.0.2
    支持文章百度主动推送

--- Sitemap/Plugin.php
+++ Sitemap/Plugin.php
@@ -1,13 +1,13 @@
 <?php
 
 /**
- * 全站Sitemap.xml 高度自定义设置 <br>
+ * 全站<strong style="color:#009688;">Sitemap.xml</strong>地图索引和<strong style="color:#009688;">百度文章主动推送</strong><br>
  * 请手动在robots.txt添加<a target="_blank" href="/sitemap.xml">sitemap.xml</a>指引 <br>
  * 设置教程请访问：<a style="font-weight:bold;" href="https://Oct.cn/view/66">教程地址>></a>
  *
  * @package Sitemap
  * @author 十月 Oct.cn
- * @version 1.0.1
+ * @version 1.0.2
  * @link https://Oct.cn/view/66
  */
 class Sitemap_Plugin implements Typecho_Plugin_Interface
@@ -21,10 +21,10 @@ class Sitemap_Plugin implements Typecho_Plugin_Interface
 	 */
 	public static function activate()
 	{
-		$db = Typecho_Db::get();
 		Helper::addRoute('sitemap', '/sitemap.xml', 'Sitemap_Action', 'siteMap');
 		Helper::addRoute('sitemap/sitemap_[key]', '/sitemap/sitemap_[key].xml', 'Sitemap_Action', 'siteList');
 		Helper::addRoute('sitemap/sitemap_[key]_[page]', '/sitemap/sitemap_[key]_[page].xml', 'Sitemap_Action', 'siteList');
+		Typecho_Plugin::factory('Widget_Contents_Post_Edit')->finishPublish = array(__CLASS__, 'render');
 		return ('开启成功, 插件已经成功激活!');
 	}
 	/**
@@ -51,24 +51,27 @@ class Sitemap_Plugin implements Typecho_Plugin_Interface
 	 */
 	public static function config(Typecho_Widget_Helper_Form $form)
 	{
-		$siteUrl = Typecho_Widget::widget('Widget_Options')->siteUrl;
-		$siteStatus =  new Typecho_Widget_Helper_Form_Element_Radio('siteStatus', array('1' => _t('开启'), '0' => _t('关闭')), '1', _t('开启sitemap.xml'), _t('开启后对收录更友好，sitemap地址：<a target="_blank" href="/sitemap.xml">' . $siteUrl . 'sitemap.xml</a>'));
+		$siteStatus =  new Typecho_Widget_Helper_Form_Element_Radio('siteStatus', array('1' => _t('开启'), '0' => _t('关闭')), '1', _t('开启sitemap.xml'), _t('开启后对收录更友好'));
 		$form->addInput($siteStatus);
-		$levelSite =  new Typecho_Widget_Helper_Form_Element_Radio('levelSite', array('1' => _t('不开启分级'), '0' => _t('开启分级')), '1', _t('是否分多个xml文件'), _t('百度不建议分级，但分级也可以收录。若数据量很大，打开缓慢时建议开启。可把每个xml提交给百度'));
+		$baiduPost =  new Typecho_Widget_Helper_Form_Element_Radio('baiduPost', array('1' => _t('开启'), '0' => _t('关闭')), '1', _t('开启自动推送'), _t('开启后发布完文章会自动推送文章地址给百度，加快收录。<a target="_blank" href="https://Oct.cn/view/66#百度主动推送">主动推送说明</a>'));
+		$form->addInput($baiduPost);
+		$apiUrl = new Typecho_Widget_Helper_Form_Element_Text('apiUrl', NULL, '', _t('百度推送接口地址'), _t('token变化后，请同步修改此处，否则身份校验不通过将推送失败。<a target="_blank" href="https://Oct.cn/view/66#百度主动推送">获取地址教程</a>'));
+		$form->addInput($apiUrl);
+		$levelSite =  new Typecho_Widget_Helper_Form_Element_Radio('levelSite', array('1' => _t('不开启分级'), '0' => _t('开启分级')), '1', _t('是否分多个xml文件'), _t('百度不建议分级，但分级也可以收录。若数据量很大，打开缓慢时建议开启'));
 		$form->addInput($levelSite);
 		$sitePageSize =  new Typecho_Widget_Helper_Form_Element_Radio('sitePageSize', array('200' => _t('200'), '500' => _t('500'), '1000' => _t('1000')), '200', _t('每页最多可显示'), _t('仅在开启分级下生效，建议200条,超出自动分页'));
 		$form->addInput($sitePageSize);
-		$catePriority =  new Typecho_Widget_Helper_Form_Element_Radio('catePriority', array('1' => _t('1'), '0.9' => _t('0.9'), '0.8' => _t('0.8'), '0.7' => _t('0.7'), '0.6' => _t('0.6'), '0.5' => _t('0.5')), '0.9', _t('分类页优先级'));
+		$catePriority =  new Typecho_Widget_Helper_Form_Element_Radio('catePriority', array('1' => _t('1'), '0.9' => _t('0.9'), '0.8' => _t('0.8'), '0.7' => _t('0.7'), '0.6' => _t('0.6'), '0.5' => _t('0.5')), '0.9', _t('分类页'));
 		$form->addInput($catePriority);
-		$tagPriority =  new Typecho_Widget_Helper_Form_Element_Radio('tagPriority', array('1' => _t('1'), '0.9' => _t('0.9'), '0.8' => _t('0.8'), '0.7' => _t('0.7'), '0.6' => _t('0.6'), '0.5' => _t('0.5')), '0.8', _t('标签页优先级'));
+		$tagPriority =  new Typecho_Widget_Helper_Form_Element_Radio('tagPriority', array('1' => _t('1'), '0.9' => _t('0.9'), '0.8' => _t('0.8'), '0.7' => _t('0.7'), '0.6' => _t('0.6'), '0.5' => _t('0.5')), '0.8', _t('标签页'));
 		$form->addInput($tagPriority);
-		$postPriority =  new Typecho_Widget_Helper_Form_Element_Radio('postPriority', array('1' => _t('1'), '0.9' => _t('0.9'), '0.8' => _t('0.8'), '0.7' => _t('0.7'), '0.6' => _t('0.6'), '0.5' => _t('0.5')), '0.9', _t('文章页优先级'));
+		$postPriority =  new Typecho_Widget_Helper_Form_Element_Radio('postPriority', array('1' => _t('1'), '0.9' => _t('0.9'), '0.8' => _t('0.8'), '0.7' => _t('0.7'), '0.6' => _t('0.6'), '0.5' => _t('0.5')), '0.9', _t('文章页'));
 		$form->addInput($postPriority);
-		$HomePagePriority =  new Typecho_Widget_Helper_Form_Element_Radio('HomePagePriority', array('1' => _t('1'), '0.9' => _t('0.9'), '0.8' => _t('0.8'), '0.7' => _t('0.7'), '0.6' => _t('0.6'), '0.5' => _t('0.5')), '0.8', _t('首页分页优先级'));
+		$HomePagePriority =  new Typecho_Widget_Helper_Form_Element_Radio('HomePagePriority', array('1' => _t('1'), '0.9' => _t('0.9'), '0.8' => _t('0.8'), '0.7' => _t('0.7'), '0.6' => _t('0.6'), '0.5' => _t('0.5')), '0.8', _t('首页分页'));
 		$form->addInput($HomePagePriority);
-		$CatePagePriority =  new Typecho_Widget_Helper_Form_Element_Radio('CatePagePriority', array('1' => _t('1'), '0.9' => _t('0.9'), '0.8' => _t('0.8'), '0.7' => _t('0.7'), '0.6' => _t('0.6'), '0.5' => _t('0.5')), '0.7', _t('分类分页优先级'));
+		$CatePagePriority =  new Typecho_Widget_Helper_Form_Element_Radio('CatePagePriority', array('1' => _t('1'), '0.9' => _t('0.9'), '0.8' => _t('0.8'), '0.7' => _t('0.7'), '0.6' => _t('0.6'), '0.5' => _t('0.5')), '0.7', _t('分类分页'));
 		$form->addInput($CatePagePriority);
-		$pagesPriority =  new Typecho_Widget_Helper_Form_Element_Radio('pagesPriority', array('1' => _t('1'), '0.9' => _t('0.9'), '0.8' => _t('0.8'), '0.7' => _t('0.7'), '0.6' => _t('0.6'), '0.5' => _t('0.5')), '0.8', _t('独立页面优先级'));
+		$pagesPriority =  new Typecho_Widget_Helper_Form_Element_Radio('pagesPriority', array('1' => _t('1'), '0.9' => _t('0.9'), '0.8' => _t('0.8'), '0.7' => _t('0.7'), '0.6' => _t('0.6'), '0.5' => _t('0.5')), '0.8', _t('独立页面'));
 		$form->addInput($pagesPriority);
 	}
 	/**
@@ -80,5 +83,53 @@ class Sitemap_Plugin implements Typecho_Plugin_Interface
 	 */
 	public static function personalConfig(Typecho_Widget_Helper_Form $form)
 	{
+	}
+
+	/**
+	 * 插件实现方法
+	 * 
+	 * @access public
+	 * @return void
+	 */
+	public static function render($contents, $widget)
+	{
+		$Sitemap = Typecho_Widget::widget('Widget_Options')->Plugin('Sitemap');
+		/* 允许自动推送 */
+		if ($Sitemap->baiduPost == 1) {
+			$url = $widget->permalink;
+			/* 没有填写接口地址 */
+			if (empty($Sitemap->apiUrl)) {
+				$postMsg = ' -- 百度推送【失败】,请先设置插件中的接口地址；';
+			} else {
+				$client = Typecho_Http_Client::get();
+				$postMsg = ' -- 百度推送【失败】';
+				if ($client) {
+					$client->setData(implode(PHP_EOL, [$url]))
+						->setHeader('Content-Type', 'text/plain')
+						->setTimeout(30)
+						->send($Sitemap->apiUrl);
+
+					$status = $client->getResponseStatus();
+					$res = $client->getResponseBody();
+					$res = json_decode($res, true);
+					if ($status == 200 && $res['success'] == 1) {
+						$postMsg = ' -- 百度推送【成功】,今日剩余次数' . $res['remain'];
+					}
+					if (!empty($res['not_same_site'])) {
+						$postMsg .= '失败原因：不是本站url，推送的url和token所属的不一致，';
+					}
+
+					if (!empty($res['message'])) {
+						$postMsg .= '失败原因：' . $res['message'] . '；请检查token是否正确；';
+					}
+				} else {
+					$postMsg = ' -- 百度推送【失败】，您的服务器不支持curl请求';
+				}
+			}
+			$adminUrl = Typecho_Widget::widget('Widget_Options')->adminUrl;
+			header("refresh:0;url= " . $adminUrl . "manage-posts.php");
+			Typecho_Widget::widget('Widget_Notice')->set(_t('文章 "<a href="%s">%s</a>" 已经发布 ' . $postMsg, $url, $widget->title), 'success');
+			die();
+		}
 	}
 }
